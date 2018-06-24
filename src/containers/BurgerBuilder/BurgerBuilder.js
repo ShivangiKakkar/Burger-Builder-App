f1f1ae6b9d846d3e400a5e8d5ca4f63018f7f19e
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-06-24 15:48:59 +0530

    started implementing BurgerBuilder container

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class BurgerBuilder extends Component {
+    render () {
+        return (
+            <Aux>
+                <div>Burger</div>
+                    <div>Build Controls</div>
+                </Aux>
+
+        );
+    }
+}
+export default BurgerBuilder;
