546f9e4838593ffe60f6542f03e3d7189ddb9f8e
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-06-25 09:24:42 +0530

    Display - add ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,20 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igkey => {
         return [...Array(props.ingredients[igkey])].map((_, i) => {
             return <BurgerIngredient key={igkey + i} type={igkey}/>;
         });
-    });
+    })
+     .reduce((arr, el )=> {
+         return arr.concat(el)
+     },[]);
+     //console.log(transformedIngredients);
+    if(transformedIngredients.length === 0) {
+         transformedIngredients = <p>Please start adding ingredients! </p>;
+    }
+
     return(
       <div className={classes.Burger}>
         <BurgerIngredient type="bread-top" />
