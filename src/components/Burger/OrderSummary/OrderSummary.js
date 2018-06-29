da2dae0bfb0f11634b134e3af360f8901501d210
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-06-29 21:15:44 +0530

    error review

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,29 @@
 import React from 'react';
+
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-      .map(igKey => {
-          return (
-                  <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                 </li> );
-      });
+const orderSummary = ( props ) => {
+    const ingredientSummary = Object.keys( props.ingredients )
+        .map( igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                </li> );
+        } );
+
     return (
         <Aux>
-            
             <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients: </p>
+            <p>A delicious burger with the following ingredients:</p>
             <ul>
-                {ingredientSummary} 
+                {ingredientSummary}
             </ul>
             <p>Continue to Checkout?</p>
-            <button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</button>
-            <button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</button>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </Aux>
     );
-
 };
 
 export default orderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,21 @@
 import React from 'react';
+
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
+const modal = ( props ) => (
     <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div 
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+        <div
             className={classes.Modal}
             style={{
                 transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1': '0'
+                opacity: props.show ? '1' : '0'
             }}>
-            {props.childern}
+            {props.children}
         </div>
-    </Aux>    
-
+    </Aux>
 );
 
 export default modal;
