873511f9f59931eb3c99ce4f2b60fc2aa8731f5b
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-06-29 19:55:38 +0530

    adding backdrop

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -72,6 +72,10 @@ class BurgerBuilder extends Component {
     purchaseHandler = () => {
         this.setState({purchasing: true});
     }
+
+    purchaseCancelHandler = () => {
+         this.setState({purchasing: false});
+    }
     render () {
         const disabledInfo = {
             ...this.state.ingredients
@@ -83,7 +87,7 @@ class BurgerBuilder extends Component {
         
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients = {this.state.ingredients}/> 
                     </Modal>
                 <Burger ingredients={this.state.ingredients}/>
