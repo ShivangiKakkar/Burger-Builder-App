17d2e7df0822ec7a7b27ece1cc8b367e03a7d55c
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-07-02 23:44:06 +0530

    Added Navigation items

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import classes from './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
-    <ul></ul>
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" active>Burger Builder</NavigationItem>
+        <NavigationItem link="/">Checkout</NavigationItem>
+    </ul>
 
 );
 
-export default naigationItems;
+export default navigationItems;
