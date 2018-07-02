e3b275194b792a446de0d96a6e46a121694edfa5
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-07-02 23:07:38 +0530

    ..

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const navigationItems = () => (
+    <ul></ul>
+
+);
+
+export default naigationItems;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -44,7 +44,7 @@ class BurgerBuilder extends Component {
         const priceAddition = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition ;
-
+        
 
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
         this.updatePurchaseState(updatedIngredients);
