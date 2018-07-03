83e754584e9d0bf0d57b055888377c46138efbe3
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-07-03 12:07:15 +0530

    added more responsive adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -9,7 +9,7 @@ const toolbar = () => (
         <div className={classes.Logo}>
             <Logo />
         </div>    
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems/>
         </nav>
     </header>
