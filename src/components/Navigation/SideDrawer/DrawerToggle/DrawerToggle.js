388f9f9dbc352fd68aeae969ee41d5c48efaba52
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-07-05 12:18:41 +0530

    added icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import classes from './DrawerToggle.css';
 const drawerToggle = (props) => (
-     <div onClick = {props.clicked}>MENU</div>
+     <div className = {classes.DrawerToggle} onClick = {props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+     </div>
 
 );
 export default drawerToggle;
