df9a4cf5ce61997ce1065d832e2c7284a55f1e7a
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-07-15 08:31:35 +0530

    sending post requests to and adding data into database

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -78,7 +79,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+       // alert('You continue!');
+       const order = {
+        ingredients: this.state.ingredients,
+        price: this.state.totalPrice,
+        customer: {
+            name:'Shivi',
+            address: {
+                street: '16-A',
+                zipCode: '140123',
+                country: 'India',
+                state: 'Punjab'
+            },
+            email: 'loveyou@anurag.com'
+             }, 
+            deliveryMethod: 'fastest'
+       }
+       axios.post('/orders.json', order)
+       .then()response => console.log(response)
+       .catch(error => console.log(error));
+
     }
     render () {
         const disabledInfo = {
