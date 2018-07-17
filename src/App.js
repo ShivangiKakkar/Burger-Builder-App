10b71f932959180042e393c95f7da300be4b461a
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-07-17 06:07:10 +0530

    adjusting burger size

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       <div>
         <Layout>
           <BurgerBuilder /> 
-          <Checkout />
+          <Checkout /> 
           </Layout>
 
       </div>
--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -7,7 +7,7 @@ const checkoutSummary = (props) => {
     return (
         <div className = {classes.CheckoutSummary}>
             <h1>We hope it tastes well!</h1>
-                <div style={{width: '300px', height: '300px', margin: 'auto'}}>
+                <div style={{width: '100%', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients}/>
                 </div>
                 <Button 
