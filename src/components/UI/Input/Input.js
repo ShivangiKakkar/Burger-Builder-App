9b3b6d36ad05a3a1acd25747368e039b2e35ea08
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-07-22 03:39:30 +0530

    Handling user input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -9,19 +9,22 @@ const input = (props) => {
             inputElement = <input 
                 className = {classes.InputElement} 
                 {...props.elementConfig} 
-                value={props.value}/>;
+                value={props.value}
+                onChange={props.changed}/>;
             break;
         case ('textarea'):
             inputElement = <textarea 
                 className = {classes.InputElement} 
                 {...props.elementConfig} 
-                value={props.value}/>;
+                value={props.value}
+                onChange={props.changed}/>;
             break;
         case ('select'):
             inputElement = (
                 <select 
                     className={classes.InputElement}
-                    value={props.value}>
+                    value={props.value}
+                    onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
                         {option.displayValue}
@@ -34,7 +37,8 @@ const input = (props) => {
             inputElement = <input 
                 className = {classes.InputElement} 
                 {...props.elementConfig} 
-                value={props.value}/>;
+                value={props.value}
+                onChange={props.changed}/>;
     }
     return (
         <div className={classes.Input}>
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -79,6 +79,20 @@ class ContactData extends Component  {
                     this.setState({loading: false });
                    });
     }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        //console.log(event.target.value);
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm});
+    }
+
     render () {
         const formElementsArray = [];
         for (let key in this.state.orderForm) {
@@ -95,7 +109,8 @@ class ContactData extends Component  {
                             key={formElement.id}
                             elementType={formElement.config.elementType}
                             elementConfig={formElement.config.elementConfig}
-                            value={formElement.config.value}/>
+                            value={formElement.config.value}
+                            changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                     ))}
                     <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
