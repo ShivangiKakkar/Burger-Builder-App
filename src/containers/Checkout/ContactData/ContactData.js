9b71723d2620c159758e8fa24ede4c59bc8e6724
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-07-22 05:01:22 +0530

    Handling form submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -65,10 +65,15 @@ class ContactData extends Component  {
         event.preventDefault();
           //console.log(this.props.ingredients);
                   this.setState({loading: true});
+                  const formData = {};
+                  for (let formElementIdentifier in this.state.orderForm) {
+                      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+                  }
                    // alert('You continue!');
                    const order = {
                     ingredients: this.props.ings,
-                    price: this.props.price
+                    price: this.props.price,
+                    orderData: formData
                    }
                    axios.post('/orders.json', order)
                         .then(response => {
@@ -102,7 +107,7 @@ class ContactData extends Component  {
             });
         }
         let form = (
-                <form>
+                <form onSubmit={this.orderHandler}>
                    
                     {formElementsArray.map(formElement => (
                         <Input 
@@ -112,7 +117,7 @@ class ContactData extends Component  {
                             value={formElement.config.value}
                             changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                     ))}
-                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                    <Button btnType="Success">ORDER</Button>
                 </form>
         );
         if(this.state.loading) {
