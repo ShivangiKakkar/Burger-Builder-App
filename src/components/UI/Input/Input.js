810829becfe750ab03cbec71d3b10ab8ab071394
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-07-22 03:23:14 +0530

    Adding a dropdown component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -17,6 +17,19 @@ const input = (props) => {
                 {...props.elementConfig} 
                 value={props.value}/>;
             break;
+        case ('select'):
+            inputElement = (
+                <select 
+                    className={classes.InputElement}
+                    value={props.value}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                        {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
+            break;
         default:
             inputElement = <input 
                 className = {classes.InputElement} 
