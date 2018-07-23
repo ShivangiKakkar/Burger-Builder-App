b53d34c4352e69e9e4c01b7b3683a2df055e2220
ShivangiKakkare <shivangikakkar15@gmail.com>
2018-07-23 23:43:51 +0530

    Adding an Auth form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
   // state = {
@@ -21,6 +22,7 @@ class App extends Component {
           <Switch>
             <Route path = "/checkout" component = {Checkout} />
             <Route path = "/orders" component = {Orders} />
+            <Route path = "/auth" component = {Auth} />
             <Route path = "/" exact component = {BurgerBuilder} />
           </Switch>
           </Layout>
